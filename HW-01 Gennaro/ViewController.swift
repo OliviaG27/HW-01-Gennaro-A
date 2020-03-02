e68b1adcfc922188f8cc2f0fd757f493c0e94922
Olivia Gennaro <o.gennaro@setonhill.edu>
2020-03-02 12:27:53 -0500

    Initial Commit

---
+++ HW-01 Gennaro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW-01 Gennaro
+//
+//  Created by Olivia Gennaro on 3/2/20.
+//  Copyright © 2020 Olivia Gennaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW-01 GennaroTests/HW_01_GennaroTests.swift
@@ -0,0 +1,34 @@
+//
+//  HW_01_GennaroTests.swift
+//  HW-01 GennaroTests
+//
+//  Created by Olivia Gennaro on 3/2/20.
+//  Copyright © 2020 Olivia Gennaro. All rights reserved.
+//
+
+import XCTest
+@testable import HW_01_Gennaro
+
+class HW_01_GennaroTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
